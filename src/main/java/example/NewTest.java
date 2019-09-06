4ae91b6cfdbbc4aedaa263fffcddbb22747fb75d
fadibarcelona <41168516+fadibarcelona@users.noreply.github.com>
2019-09-06 22:10:49 +0500

    Update NewTest.java

--- src/main/java/example/NewTest.java
+++ src/main/java/example/NewTest.java
@@ -1,34 +1,26 @@
-package example;		
+package example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import org.openqa.selenium.By;		
-import org.openqa.selenium.WebDriver;		
-import org.openqa.selenium.firefox.FirefoxDriver;		
-import org.testng.Assert;		
-import org.testng.annotations.Test;	
-import org.testng.annotations.BeforeTest;	
-import org.testng.annotations.AfterTest;	
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import java.net.URL;	
-public class NewTest {		
-	    private WebDriver driver;		
+public class jenkins_demo
+{
 
-	    	  @Test
-	    	  public void openMyBlog() {
-	    	 driver.get("https://www.softwaretestingmaterial.com/");
-	    	  }
-	    	  
-	    	  @BeforeTest
-	    	  public void beforeTest() {
-	    			System.setProperty("browser.gecko.driver", System.getProperty("/var/lib/jenkins/workspace/servicenow/")+"/geckodriver");
-//	    	   System.setProperty("webdriver.gecko.driver", "C:\\Users\\taha.patel\\eclipse-workspace2\\SampleProject\\geckodriver.exe");
-	    	   driver = new FirefoxDriver();
-	    	   
-	    	  }
-	    	 
-	    	  @AfterTest
-	    	  public void afterTest() {
-	    	   driver.quit();
-	    	  }	
+@Test
+publicvoidtestgooglrsearch(){
+
+WebDriver driver = newFirefoxDriver();
+//it will open the goggle page
+driver.get("http://google.in"); 
+//we expect the title “Google “ should be present 
+String Expectedtitle = "Google";
+//it will fetch the actual title 
+String Actualtitle = driver.getTitle();
+System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+//it will compare actual title and expected title
+Assert.assertEquals(Actualtitle, Expectedtitle);
+//print out the result
+System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+ }
 }
-
