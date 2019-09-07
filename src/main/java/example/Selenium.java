2ab8c148d6616fe04304b239bdefe1465d83c2f1
fadibarcelona <41168516+fadibarcelona@users.noreply.github.com>
2019-09-07 05:42:02 +0500

    Update Selenium.java

--- src/main/java/example/Selenium.java
+++ src/main/java/example/Selenium.java
@@ -1,20 +1,28 @@
 package example;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
 public class Selenium {
 public static void main(String[] args) {
-SSystem.setProperty("browser.gecko.driver", System.getProperty("/var/lib/jenkins/workspace/servicenow/")+"/geckodriver");
+//System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/workspace/selenium-free/geckodriver"); // <– Change this path
+System.setProperty("browser.gecko.driver", System.getProperty("/var/lib/jenkins/workspace/selenium-free/")+"/geckodriver");
 WebDriver driver = new FirefoxDriver();
-
-String baseUrl = “https://experitest.com/free-trial/”;
-String expectedTitle = “Free trial”;
-String actualTitle = “”;
+String baseUrl = "https://experitest.com/free-trial/";
+String expectedTitle = "Free trial";
+String actualTitle = "";
 driver.get(baseUrl);
 actualTitle = driver.getTitle();
 if (actualTitle.contentEquals(expectedTitle)){
-System.out.println(“TEST PASSED!”);
+System.out.println("TEST PASSED!");
 } else {
-System.out.println(“TEST FAILED”);
+System.out.println("TEST FAILED");
 }
 driver.close();
 }
